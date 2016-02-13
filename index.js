951f1da464073b055bc3a51b564397426bfb957c
gugani <gustavo.glezf@gmail.com>
2016-02-13 20:18:36 +0000

    El backend pasa los discos y cliente web actualiza el list-group. Sólo añade.

--- index.js
+++ index.js
@@ -46,35 +46,52 @@ io.sockets.on('connection', function (socket) {
 
     //Mensajes de eventos de control
     socket.on('guievent', function (data) {
-        console.log("gui event received")
-        console.log("Type: " + data.type + " Subtype: " + data.subtype + " Value: " + data.val);
+        console.log("gui event received");
+        console.log("Type: " + data.type + " Name: " + data.name + " Value: " + data.val);
+        if (data.name == "refresh"){
+            get_linux_partitions()
+        }
     });
 });
 
 
 // http://nodejs.org/api.html#_child_processes
-var util = require('util')
-var exec = require('child_process').exec;
-var child;
+// var util = require('util')
+// var exec = require('child_process').exec;
+// var child;
 
-// executes `pwd`
-child = exec("cat /proc/partitions", function (error, stdout, stderr) {
-  console.log('stdout: ' + stdout);
-  console.log('stderr: ' + stderr);
-  if (error !== null) {
-    console.log('exec error: ' + error);
-  }
-});
-
-fs = require('fs')
-fs.readFile('/etc/hosts', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-});
-
+// // executes `pwd`
+// child = exec("cat /proc/partitions", function (error, stdout, stderr) {
+//   console.log('stdout: ' + stdout);
+//   console.log('stderr: ' + stderr);
+//   if (error !== null) {
+//     console.log('exec error: ' + error);
+//   }
+// });
 
 
 
 //Functions--------------------------------------------------------------------------------------------------------------
+
+/**
+ * Get Linux drives
+ * */
+function get_linux_partitions(){
+  fs = require('fs')
+  fs.readFile('partitions', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    else{
+      var lines = data.split('\n');
+      for (i = 0; i < lines.length; i++) {
+        if (lines[i].search("sd[a-z]$") != -1){
+          // console.log(lines[i]);
+          var details = lines[i].split(" ");
+          console.log("Name: " + details[details.length - 1] + " - Size: " + details[details.length - 2]);
+          io.sockets.emit('serverevent', { type: 'adddisk', name: details[details.length - 1], size: details[details.length - 2]});
+        }
+      }
+    }
+  });
+}
--- public/js/clonezillagui.js
+++ public/js/clonezillagui.js
@@ -1,10 +1,27 @@
-var socket = io.connect('http://192.168.1.103:3000');
+var socket = io.connect('http://127.0.0.1:3000');
 console.log("iosocket connected")
 
-
+//Server events messages
 $('#refreshbutton').on('click', function(event) {
   event.preventDefault(); // To prevent following the link (optional)
   console.log("clicked!");
-  socket.emit('guievent', { type: 'command', subtype: 'projinput', val: "hdmi1" } );
-  socket.send({ type: 'command', subtype: 'projinput', val: "hdmi1" } );
+  socket.emit('guievent', { type: 'command', name: 'refresh', val: 1 } );
+  // socket.send({ type: 'command', subtype: 'projinput', val: "hdmi1" } );
 });
+
+//Server events messages
+socket.on('serverevent', function (data) {
+    console.log(data);
+    if (data.type == "adddisk"){
+        adddisk(data.name, data.size);
+    }
+
+    // console.log("LOG: Mensaje recibido. Type: " + data.type + " Subtype: " + data.subtype + " Value: " + data.val);
+});
+
+
+//Utils
+function adddisk(name, size){
+  $("#hdlist").append('<a id="' + name + '"href="#" class="list-group-item">' + name + ' - ' + size + ' Kb</a>');
+  console.log("adddisk");
+}
